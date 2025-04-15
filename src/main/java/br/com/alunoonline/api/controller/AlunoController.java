69428a1a6aaf1b0739dd5452f422ce106bc8918b
Albanezfxp <gabrielalbanez2701@gmail.com>
2025-04-15 15:44:36 -0300

    created routers getById and delete

--- src/main/java/br/com/alunoonline/api/controller/AlunoController.java
+++ src/main/java/br/com/alunoonline/api/controller/AlunoController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/alunos")
 public class AlunoController {
@@ -16,5 +19,23 @@ public class AlunoController {
     @ResponseStatus(HttpStatus.CREATED)
     public void create(@RequestBody Aluno aluno) {
         alunoService.create(aluno);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Aluno> getAll() {
+        return alunoService.getAllStudent();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<Aluno> getStudentsById(@PathVariable Long id ) {
+        return alunoService.getStudentById(id);
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public String deleteStudents(@PathVariable Long id) {
+        return alunoService.delete(id);
     }
 }
--- src/main/java/br/com/alunoonline/api/service/AlunoService.java
+++ src/main/java/br/com/alunoonline/api/service/AlunoService.java
@@ -5,6 +5,10 @@ import br.com.alunoonline.api.repository.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 
 @Service
 public class AlunoService {
@@ -14,5 +18,23 @@ public class AlunoService {
     public void create(Aluno aluno) {
         alunoRepository.save(aluno);
     }
+
+    public List<Aluno> getAllStudent() {
+       return alunoRepository.findAll();
+    }
+
+    public Optional<Aluno> getStudentById(Long id) {
+        return alunoRepository.findById(id);
+    }
+
+    public String delete(Long id) {
+        Optional<Aluno>alunoDeleted = alunoRepository.findById(id);
+        if (alunoDeleted.isEmpty()) {
+            return "Aluno não encontrado";
+        }
+        alunoRepository.delete(alunoDeleted.get());
+        return "Aluno " + alunoDeleted.get().getName() + " deleteado";
+    }
+
 
 }
